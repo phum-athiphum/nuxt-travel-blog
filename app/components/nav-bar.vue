9d7662436d82b1b5d50ad3c0e2c5bcdbba0d8d88
phum-athiphum <athiphumm@gmail.com>
2025-11-16 22:13:34 +0700

    ✨add hero section and navbar

---
+++ app/components/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl text-red-500">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign in<Icon name="tabler:brand-github" size="24" /></a>
+    </div>
+  </div>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <NavBar />
+    <main class="flex-1 flex flex-col">
+      <slot />
+    </main>
+  </div>
+</template>
