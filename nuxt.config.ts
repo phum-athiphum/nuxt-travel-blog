30d7d3d61a9da98863eb84fb215d72ccafe29f07
Phum <>
2025-11-16 13:05:03 +0700

    ✨add tailwind css and daisy ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  // บอกว่าทุกหน้าของ nuxt ต้องโหลดอันนี้
+  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
